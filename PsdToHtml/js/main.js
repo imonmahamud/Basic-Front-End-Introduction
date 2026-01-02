c24bcd9ae0186b86155385b7990e5b2168b92721
Mahamud Hasan Emon <imonmahamud495@gmail.com>
2026-01-02 21:37:31 +0600

    Here I am desinging a website from a psd file so far I have complited five section.

--- PsdToHtml/js/main.js
+++ PsdToHtml/js/main.js
@@ -1,1 +1,19 @@
-ECHO is on.
+$(document).ready(function () {
+    $('.slider-active').owlCarousel({
+        loop: true,
+        dots: true,
+        nav: true,
+        navText: ['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+});
