55dafdaa027fb22f5d7d662ebdecbd216737b544
Mahamud Hasan Emon <imonmahamud495@gmail.com>
2026-01-04 00:47:27 +0600

    Entend some wonderful section here

--- PsdToHtml/js/main.js
+++ PsdToHtml/js/main.js
@@ -16,4 +16,26 @@ $(document).ready(function () {
             }
         }
     })
+    $('.project-active').owlCarousel({
+        loop: true,
+        dots: true,
+        nav: true,
+        navText: ['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
+
+$('.vidoe-popup').magnificPopup({
+  type: 'iframe'
+  // other options
+});
 });
