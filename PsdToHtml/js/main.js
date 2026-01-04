401748c4e032f35940ae888922dcb7cb1f88f90f
Mahamud Hasan Emon <imonmahamud495@gmail.com>
2026-01-04 21:51:26 +0600

    Almost finish the design for page 1

--- PsdToHtml/js/main.js
+++ PsdToHtml/js/main.js
@@ -38,4 +38,39 @@ $('.vidoe-popup').magnificPopup({
   type: 'iframe'
   // other options
 });
+    $('.blog-active').owlCarousel({
+        loop: true,
+        dots: true,
+        nav: true,
+        navText: ['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+    $('.brand-active').owlCarousel({
+        loop: true,
+        dots: false,
+        nav: false,
+        margin:50,
+        navText: ['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 6
+            }
+        }
+    })
 });
